c49dfbd89e084e5c63a357f5599021b8ea37cda6
Sean Hickey <shickey.dev@gmail.com>
2014-10-04 16:15:18 -0500

    Began implementing new simulator

---
+++ new_simulator.js
@@ -0,0 +1,339 @@
+var hmmm = require('./hmmm_language');
+
+
+function HmmmSimulator(inHandler, outHandler, errHandler) {
+  
+  'use strict'
+  
+  var machine = this;
+  
+  var NUM_REGISTERS = 16;
+  var RAM_SIZE      = 256;
+  
+  var states = Object.freeze({
+    EMPTY   : 'empty',
+    READY   : 'ready',
+    RUN     : 'run',
+    PAUSE   : 'pause',
+    HALT    : 'halt',
+    ERROR   : 'error'
+  });
+  
+  //*********************************************
+  // User Defined Input/Output Functions
+  //*********************************************
+  this.inHandler  = inHandler;
+  this.outHandler = outHandler;
+  this.errHandler = errHandler;
+  
+  //*********************************************
+  // Public State
+  //*********************************************
+  this.registers = [];
+  this.ram       = [];
+  this.pc        = 0;
+  this.ir        = 0;
+  this.boundary  = 0;
+  this.state     = states.EMPTY;
+  
+  for (var i = 0; i < NUM_REGISTERS; ++i) {
+    this.registers.push(0);
+  }
+  
+  for (var j = 0; j < RAM_SIZE; ++j) {
+    this.ram.push(0);
+  }
+  
+  //*********************************************
+  // Private Methods
+  //*********************************************
+  function decodeBinaryInstruction(binInst) {
+    var encoded = binInst;
+    var decoded = {
+      operation : null,
+      args      : []
+    };
+    
+    // Find the correct operation by iterating over the
+    // list of instructions in order of precedence
+    hmmm.opcodePrecedence.some(function(operation){
+      var opcode = parseInt(hmmm.opcodes[operation].opcode, 2);
+      var mask   = parseInt(hmmm.opcodes[operation].mask,   2);
+      if ((encoded & mask) === opcode) {
+        // We found the right operation
+        decoded.operation = operation;
+        return true;
+      }
+    });
+    
+    if (!decoded.operation) {
+      // TODO Throw, we couldn't decode the operation
+    }
+    
+    // Parse Arguments
+    var signature = hmmm.signatures[decoded.operation];
+    encoded = encoded << 4;
+    for (var i = 0; i < signature.length; ++i) {
+      var type = signature.charAt(i);
+      if (type === "r") {
+        var reg = (encoded & 0xf000) >> 12;
+        decoded.args.push("r" + reg);
+        encoded = encoded << 4;
+      }
+      else if (type === "s") {
+        var num = (encoded & 0xff00) >> 8;
+        if ((num & 0x80) !== 0) {
+          num -= 256; // Account for sign
+        }
+        decoded.args.push(num);
+        encoded = encoded << 8;
+      }
+      else if (type === "u") {
+        var num = (encoded & 0xff00) >> 8;
+        decoded.args.push(num);
+        encoded = encoded << 8;
+      }
+      else if (type === "z") {
+        encoded = encoded << 4;
+      }
+      else if (type === "n") {
+        decoded.args.push(encoded);
+        encoded = encoded << 16;
+      }
+      else {
+        // TODO throw, internal inconsistency
+      }
+    }
+    return decoded;
+  }
+  
+  function executeInstruction(operation, args) {
+    // Bump the instruction number
+    machine.ip += 1
+    
+    // Unpack Arguments
+    var rx, ry, rz, n;
+    var signature = hmmm.signatures[operation];
+    
+    var argNum = 0; // Must keep track separately from loop iteration due
+                    // to signatures containing 'z'
+    for (var i = 0; i < signature.length; ++i) {
+      var type = signature.charAt(i);
+      if (type === "r") {
+        var arg = +(args[argNum].slice(1));
+        // Declare registers in order
+        if (rx === undefined) {
+          rx = arg;
+        }
+        else if (ry === undefined) {
+          ry = arg;
+        }
+        else if (rz === undefined) {
+          rz = arg;
+        }
+        else {
+          // TODO Internal inconsistency error
+        }
+        argNum += 1;
+      }
+      else if (type === "u" || type === "s") {
+        n = +(args[argNum]);
+        argNum += 1;
+      }
+      else if (type === "z"){
+        // Do nothing
+        // And don't increment argNum
+      }
+      else {
+        // TODO Throw internal inconsistency error
+      }
+    }
+    
+    // Now actually execute the correct operation
+    if (op === "halt") {
+      machine.state = states.HALT;
+    }
+    else if (op === "read") {
+      var userInput = machine.inHandler();
+      machine.register[rx] = userInput;
+    }
+    else if (op === "write") {
+      var data = machine.registers[rx];
+      machine.outHandler(data);
+    }
+    else if (op === "jumpr") {
+      var data = machine.registers[rx];
+      machine.pc = data;
+    }
+    else if (op === "setn") {
+      machine.registers[rx] = n;
+    }
+    else if (op === "loadn") {
+      var data = getRam(n);
+      machine.registers[rx] = data;
+    }
+    else if (op === "storen") {
+      var data = machine.registers[rx];
+      machine.ram[n] = data;
+    }
+    else if (op === "loadr") {
+      var address = machine.registers[ry];
+      var data = getRam(address);
+      machine.registers[rx] = data;
+    }
+    else if (op === "storer") {
+      var data = machine.registers[rx];
+      var address = machine.registers[ry];
+      machine.ram[address] = data;
+    }
+    else if (op === "addn") {
+      var data = machine.registers[rx];
+      machine.registers[rx] = data + n;
+    }
+    else if (op === "nop") {
+      // Do nothing
+    }
+    else if (op === "copy") {
+      var data = machine.registers[ry];
+      machine.registers[rx] = data;
+    }
+    else if (op === "add") {
+      var data1 = machine.registers[ry];
+      var data2 = machine.registers[rz];
+      machine.registers[rx] = data1 + data2;
+    }
+    else if (op === "neg") {
+      var data = machine.registers[ry];
+      machine.registers[rx] = -data;
+    }
+    else if (op === "sub") {
+      var data1 = machine.registers[ry];
+      var data2 = machine.registers[rz];
+      machine.registers[rx] = data1 - data2;
+    }
+    else if (op === "mul") {
+      var data1 = machine.registers[ry];
+      var data2 = machine.registers[rz];
+      machine.registers[rx] = data1 * data2;
+    }
+    else if (op === "div") {
+      var data1 = machine.registers[ry];
+      var data2 = machine.registers[rz];
+      machine.registers[rx] = parseInt(data1 / data2);
+    }
+    else if (op === "mod") {
+      var data1 = machine.registers[ry];
+      var data2 = machine.registers[rz];
+      machine.registers[rx] = data1 % data2;
+    }
+    else if (op === "jumpn") {
+      machine.pc = n;
+    }
+    else if (op === "calln") {
+      var nextInst = machine.pc; // We've already bumped at this point
+      machine.registers[rx] = nextInst;
+      machine.pc = n;
+    }
+    else if (op === "jeqzn") {
+      var data = machine.registers[rx];
+      if (data === 0) {
+        machine.pc = n;
+      }
+    }
+    else if (op === "jnezn") {
+      var data = machine.registers[rx];
+      if (data !== 0) {
+        machine.pc = n;
+      }
+    }
+    else if (op === "jgtzn") {
+      var data = machine.registers[rx];
+      if (data > 0) {
+        machine.pc = n;
+      }
+    }
+    else if (op === "jltzn") {
+      var data = machine.registers[rx];
+      if (data < 0) {
+        machine.pc = n;
+      }
+    }
+    else {
+      // TODO BIG ERROR
+    }
+  }
+  
+  //*********************************************
+  // Public Methods
+  //*********************************************
+  this.resetMachine = function(clearProgram) {
+    machine.pc = 0;
+    machine.ir = 0;
+    
+    if (clearProgram) {
+      machine.boundary = 0;
+    }
+    
+    for (var i = 0; i < machine.registers.length; ++i) {
+      machine.registers[i] = 0;
+    }
+    
+    for (var j = machine.boundary; j < machine.ram.length; ++j) {
+      machine.ram[j] = 0;
+    }
+    
+    if (clearProgram) {
+      machine.state = states.EMPTY;
+    }
+    else {
+      machine.state = states.READY;
+    }
+    
+  }
+  
+  this.loadBinary = function(binary) {
+    var instructions = [];
+    binary.split("\n").forEach(function(line) {
+      if (line.trim() === "") {
+        return;
+      }
+      instructions.push(parseInt(line.replace(/ /g, ""), 2));
+    });
+    for (var i = 0; i < instructions.length; ++i) {
+      machine.ram[i] = instructions[i];
+    }
+    machine.boundary = instructions.length;
+    machine.resetMachine(false);
+  }
+  
+  this.runNextInstruction = function() {
+    if (machine.pc >= machine.boundary) {
+      // TODO Throw Error
+      return;
+    }
+    var binInst = machine.ram[machine.pc];
+    var decoded = machine.decodeBinaryInstruction(binInst);
+    machine.executeInstruction(decoded.operation, decoded.args);
+  }
+  
+  this.run = function() {
+    if (machine.state == states.EMPTY) {
+      // TODO error
+      return;
+    }
+    if (machine.state == states.HALT) {
+      return;
+    }
+    if (machine.state == states.ERROR) {
+      // TODO error
+      return;
+    }
+    machine.state = states.RUN;
+    while (machine.state == states.RUN) {
+      machine.runNextInstruction();
+    }
+  }
+  
+}
+
+module.exports = exports = HmmmSimulator;
