fdfe83a01f4bd3c3a06346c7fe25bd69c3768e60
Sean Hickey <shickey.dev@gmail.com>
2014-11-01 20:27:56 -0500

    scripts/update_hmmm.sh

--- scripts/update_hmmm.sh
+++ scripts/update_hmmm.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 git co master
-if [ "$?" = "0"]; then
-  
-else
-  
+if [ "$?" = "0" ]; then
+  browserify assembler.js -o $TMPDIR/hmmm-assembler.js -s HmmmAssembler
+  browserify simulator.js -o $TMPDIR/hmmm-simulator.js -s HmmmSimulator
+  git co gh-pages
 fi
