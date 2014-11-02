6cb8c06487877f4f5d0ba08765de5c7176bb53cc
Sean Hickey <shickey.dev@gmail.com>
2014-11-01 20:31:11 -0500

    Added gitignore

--- scripts/update_hmmm.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-git co master
-if [ "$?" = "0" ]; then
-  browserify assembler.js -o $TMPDIR/hmmm-assembler.js -s HmmmAssembler
-  browserify simulator.js -o $TMPDIR/hmmm-simulator.js -s HmmmSimulator
-  git co gh-pages
-fi
