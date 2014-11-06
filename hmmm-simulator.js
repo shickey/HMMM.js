27329c027029ff6404cada95ee9cbfcfceb1030b
Sean Hickey <shickey.dev@gmail.com>
2014-11-05 23:57:51 -0600

    Added accessor functions and implemented basic error handling in simulator

--- hmmm-simulator.js
+++ hmmm-simulator.js
@@ -19,6 +19,11 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
     ERROR   : 'error'
   });
   
+  var modes = Object.freeze({
+    SAFE   : 'safe',
+    UNSAFE : 'unsafe'
+  });
+  
   //*********************************************
   // User Defined Input/Output Functions
   //*********************************************
@@ -36,6 +41,7 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
   this.ir        = 0;
   this.boundary  = 0;
   this.state     = states.EMPTY;
+  this.mode      = modes.SAFE;
   
   for (var i = 0; i < NUM_REGISTERS; ++i) {
     this.registers.push(0);
@@ -48,6 +54,91 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
   //*********************************************
   // Private Methods
   //*********************************************
+  
+  function validInteger(integer) {
+    return (integer >= -32768 && integer < 32768);
+  }
+  
+  function getProgramCounter() {
+    return this.pc
+  }
+  
+  function setProgramCounter(jumpTarget) {
+    if (jumpTarget < 0 || jumpTarget >= NUM_REGISTERS) {
+      throwSimulationError("Invalid jump target");
+      return;
+    }
+    if (this.mode == modes.SAFE && jumpTarget >= this.boundary) {
+      throwSimulationError("Invalid jump target");
+      return;
+    }
+    this.pc = jumpTarget;
+  }
+  
+  function getInstructionRegister() {
+    return this.ir;
+  }
+  
+  function setInstructionRegister(binaryInst) {
+    if (!validInteger(binaryInst)) {
+      throwSimulationError("Instruction register overflow");
+    }
+    this.ir = binaryInst;
+  }
+  
+  function getRegister(register) {
+    if (register < 0 || register >= NUM_REGISTERS) {
+      throwSimulationError("Attempted to access invalid register: r" + register);
+      return;
+    }
+    return this.registers[register];
+  }
+  
+  function setRegister(register, value) {
+    if (register < 0 || register >= NUM_REGISTERS) {
+      throwSimulationError("Attempted to access invalid register: r" + register);
+      return;
+    }
+    if (!validInteger(value)) {
+      throwSimulationError("Register overflow");
+      return;
+    }
+    if (register === 0) {
+      return; // Register 0 is always 0
+    }
+    this.registers[register] = value;
+  }
+  
+  function getRam(address) {
+    if (address < 0 || address >= RAM_SIZE) {
+      throwSimulationError("Attempted to access invalid ram address: " + address);
+      return;
+    }
+    return this.ram[address];
+  }
+  
+  function setRam(address, value) {
+    if (address < 0 || address >= RAM_SIZE) {
+      throwSimulationError("Attempted to access invalid ram address: " + address);
+      return;
+    }
+    if (this.mode = modes.SAFE && address < this.boundary) {
+      throwSimulationError("Attempted to write into code segment of RAM");
+    }
+    if (!validInteger(value)) {
+      throwSimulationError("Integer overflow");
+      return;
+    }
+    this.registers[register] = value;
+  }
+  
+  function throwSimulationError(message) {
+    this.state = this.states.ERROR;
+    if (this.errHandler) {
+      this.errHandler("ERROR: " + message);
+    }
+  }
+  
   function decodeBinaryInstruction(binInst) {
     var encoded = binInst;
     var decoded = {
@@ -68,7 +159,8 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
     });
     
     if (!decoded.operation) {
-      // TODO Throw, we couldn't decode the operation
+      // We couldn't decode the operation
+      return;
     }
     
     // Parse Arguments
@@ -102,7 +194,8 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
         encoded = encoded << 16;
       }
       else {
-        // TODO throw, internal inconsistency
+        // Internal inconsistency error
+        return;
       }
     }
     return decoded;
@@ -130,7 +223,7 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
           rz = arg;
         }
         else {
-          // TODO Internal inconsistency error
+          throwSimulationError("Internal inconsistency error");
         }
         argNum += 1;
       }
@@ -143,7 +236,7 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
         // And don't increment argNum
       }
       else {
-        // TODO Throw internal inconsistency error
+        throwSimulationError("Internal inconsistency error");
       }
     }
     
@@ -154,116 +247,132 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
     }
     
     // If we didn't halt, bump the instruction number
-    machine.pc += 1
+    setProgramCounter(getProgramCounter() + 1);
     
     // If we didn't halt, execute the correct instruction
     if (operation === "read") {
       var userInput = machine.inHandler();
-      machine.registers[rx] = userInput;
+      var validInput = /^-?[0-9]+$/.test(userInput) || /^-?0[xX][0-9a-fA-F]+$/.test(arg);
+      if (!validInput) {
+        throwSimulationError("Invalid user input");
+        return;
+      };
+      var parsedInput = parseInt(userInput);
+      setRegister(rx, parsedInput);
     }
     else if (operation === "write") {
-      var data = machine.registers[rx];
-      machine.outHandler(data);
+      var data = getRegister(rx);
+      if (machine.outHandler) {
+        machine.outHandler(data);
+      }
     }
     else if (operation === "jumpr") {
-      var data = machine.registers[rx];
-      machine.pc = data;
+      var data = getRegister(rx);
+      setProgramCounter(data);
     }
     else if (operation === "setn") {
-      machine.registers[rx] = n;
+      setRegister(rx, n);
     }
     else if (operation === "loadn") {
       var data = getRam(n);
-      machine.registers[rx] = data;
+      setRegister(rx, data);
     }
     else if (operation === "storen") {
-      var data = machine.registers[rx];
-      machine.ram[n] = data;
+      var data = getRegister(rx);
+      setRam(n, data);
     }
     else if (operation === "loadr") {
-      var address = machine.registers[ry];
+      var address = getRegister(ry);
       var data = getRam(address);
-      machine.registers[rx] = data;
+      setRegister(rx, data);
     }
     else if (operation === "storer") {
-      var data = machine.registers[rx];
-      var address = machine.registers[ry];
-      machine.ram[address] = data;
+      var data = getRegister(rx);
+      var address = getRegister(ry);
+      setRam(address, data);
     }
     else if (operation === "addn") {
-      var data = machine.registers[rx];
-      machine.registers[rx] = data + n;
+      var data = getRegister(rx);
+      setRegister(rx, data + n);
     }
     else if (operation === "nop") {
       // Do nothing
     }
     else if (operation === "copy") {
-      var data = machine.registers[ry];
-      machine.registers[rx] = data;
+      var data = getRegister(ry);
+      setRegister(rx, data);
     }
     else if (operation === "add") {
-      var data1 = machine.registers[ry];
-      var data2 = machine.registers[rz];
-      machine.registers[rx] = data1 + data2;
+      var data1 = getRegister(ry);
+      var data2 = getRegister(rz);
+      setRegister(rx, data1 + data2);
     }
     else if (operation === "neg") {
-      var data = machine.registers[ry];
-      machine.registers[rx] = -data;
+      var data = getRegister(ry);
+      setRegister(rx, -data);
     }
     else if (operation === "sub") {
-      var data1 = machine.registers[ry];
-      var data2 = machine.registers[rz];
-      machine.registers[rx] = data1 - data2;
+      var data1 = getRegister(ry);
+      var data2 = getRegister(rz);
+      setRegister(rx, data1 - data2);
     }
     else if (operation === "mul") {
-      var data1 = machine.registers[ry];
-      var data2 = machine.registers[rz];
-      machine.registers[rx] = data1 * data2;
+      var data1 = getRegister(ry);
+      var data2 = getRegister(rz);
+      setRegister(rx, data1 * data2);
     }
     else if (operation === "div") {
-      var data1 = machine.registers[ry];
-      var data2 = machine.registers[rz];
-      machine.registers[rx] = parseInt(data1 / data2);
+      var data1 = getRegister(ry);
+      var data2 = getRegister(rz);
+      if (data2 === 0) {
+        throwSimulationError("Division by zero");
+        return;
+      }
+      setRegister(rx, parseInt(data1 / data2));
     }
     else if (operation === "mod") {
-      var data1 = machine.registers[ry];
-      var data2 = machine.registers[rz];
-      machine.registers[rx] = data1 % data2;
+      var data1 = getRegister(ry);
+      var data2 = getRegister(rz);
+      if (data2 === 0) {
+        throwSimulationError("Division by zero");
+        return
+      };
+      setRegister(rx, data1 % data2);
     }
     else if (operation === "jumpn") {
-      machine.pc = n;
+      setProgramCounter(n);
     }
     else if (operation === "calln") {
-      var nextInst = machine.pc; // We've already bumped at this point
-      machine.registers[rx] = nextInst;
-      machine.pc = n;
+      var nextInst = getProgramCounter(); // We've already bumped at this point
+      setRegister(rx, nextInst);
+      setProgramCounter(n);
     }
     else if (operation === "jeqzn") {
-      var data = machine.registers[rx];
+      var data = getRegister(rx);
       if (data === 0) {
-        machine.pc = n;
+        setProgramCounter(n);
       }
     }
     else if (operation === "jnezn") {
-      var data = machine.registers[rx];
+      var data = getRegister(rx);
       if (data !== 0) {
-        machine.pc = n;
+        setProgramCounter(n);
       }
     }
     else if (operation === "jgtzn") {
-      var data = machine.registers[rx];
+      var data = getRegister(rx);
       if (data > 0) {
-        machine.pc = n;
+        setProgramCounter(n);
       }
     }
     else if (operation === "jltzn") {
-      var data = machine.registers[rx];
+      var data = getRegister(rx);
       if (data < 0) {
-        machine.pc = n;
+        setProgramCounter(n);
       }
     }
     else {
-      // TODO BIG ERROR
+      throwSimulationError("Unknown operation");
     }
   }
   
@@ -310,25 +419,26 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
   }
   
   this.runNextInstruction = function() {
-    if (machine.pc >= machine.boundary) {
-      // TODO Throw Error
+    var progCounter = getProgramCounter();
+    var binInst = getRam(progCounter);
+    setInstructionRegister(binaryInst);
+    var decoded = decodeBinaryInstruction(getInstructionRegister());
+    if (!decoded) {
+      throwSimulationError("Unable to decode instruction");
       return;
     }
-    machine.ir = machine.ram[machine.pc];
-    var decoded = decodeBinaryInstruction(machine.ir);
     executeInstruction(decoded.operation, decoded.args);
   }
   
   this.run = function(willExecute, didExecute) {
     if (machine.state == states.EMPTY) {
-      // TODO error
+      throwSimulationError("No code loaded into machine");
       return;
     }
     if (machine.state == states.HALT) {
       return;
     }
     if (machine.state == states.ERROR) {
-      // TODO error
       return;
     }
     machine.state = states.RUN;
@@ -345,6 +455,9 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
   
   this.instructionFromBinary = function(binInst) {
     var decoded = decodeBinaryInstruction(binInst);
+    if (!decoded) {
+      return;
+    }
     var instString = decoded.operation;
     for (var i = 0; i < decoded.args.length; ++i) {
       var arg = decoded.args[i];
