00c4c85fafcd0e7982f2dc2ee4a6f00ebae0ea8e
Sean Hickey <shickey.dev@gmail.com>
2014-11-08 15:13:06 -0600

    Fixed bug where invalid jump target was being thrown incorrectly

--- hmmm/hmmm-simulator.js
+++ hmmm/hmmm-simulator.js
@@ -235,7 +235,7 @@ function HmmmSimulator(inHandler, outHandler, errHandler) {
   }
   
   function setProgramCounter(jumpTarget) {
-    if (jumpTarget < 0 || jumpTarget >= NUM_REGISTERS) {
+    if (jumpTarget < 0 || jumpTarget >= RAM_SIZE) {
       throwSimulationError("Invalid jump target");
       return;
     }
