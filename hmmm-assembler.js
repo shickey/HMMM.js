86830db19e9c65a0841ab0617a519ebe8e2ba684
Sean Hickey <shickey.dev@gmail.com>
2014-11-08 15:08:45 -0600

    Fixed bug for files that do not end with a newline character

--- hmmm-assembler.js
+++ hmmm-assembler.js
@@ -149,7 +149,7 @@ function HmmmLexer() {
       
       // Ignore comments
       if (peek === '#') {
-        while (!isNewline(lookAhead(1))) {
+        while (!isNewline(lookAhead(1)) && peek !== undefined) {
           peek = getNextChar();
         }
         continue;
